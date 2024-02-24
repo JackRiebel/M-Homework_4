e5e720fd9d1821c66542bed8b1eee69d4b0eb6f6
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-24 12:36:52 -0600

    Update Car.java
    Car Constructor

--- src/m/vehicle/Car.java
+++ src/m/vehicle/Car.java
@@ -2,8 +2,20 @@ package m.vehicle;
 
 public class Car extends Vehicle {
 
-	public Car() {
+	public Car(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Car() {
+		
+	}
 
+	@Override
+	public String toCsvString() {
+		// TODO Auto-generated method stub
+		return super.toCsvString();
+	}
+	
 }
