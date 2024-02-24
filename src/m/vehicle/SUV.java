a28ed4064fa721c6eb8b7a1635fb995d88f81181
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-24 12:39:49 -0600

    Update SUV.java
    SUN constructor and ToString method.

--- src/m/vehicle/SUV.java
+++ src/m/vehicle/SUV.java
@@ -6,4 +6,16 @@ public class SUV extends Vehicle {
 		// TODO Auto-generated constructor stub
 	}
 
+	public SUV(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toCsvString() {
+		// TODO Auto-generated method stub
+		return super.toCsvString();
+	}
+
 }
