1e2d77b759a8c0bc53baa8429eb88bb4bcf38283
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-24 02:37:26 -0600

    Update Vehicle.java
    Getters, Setters, and some Constructors.

--- src/m/vehicle/Vehicle.java
+++ src/m/vehicle/Vehicle.java
@@ -15,9 +15,111 @@ public abstract class Vehicle {
 
     // Constructors, getters, setters, and other methods...
 
-    public String toCsvString() {
-        return brand + "," + make + "," + modelYear + "," + price + "," + color + ","
-               + fuelType + "," + mileage + "," + mass + "," + cylinders + ","
-               + gasTankCapacity + "," + startType;
+      
+  //Default Constructor
+    public Vehicle()
+    {
+    	
     }
+
+   //Parameterized Constructor
+	public Vehicle(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType)
+	{
+		super();
+		this.brand = brand;
+		this.make = make;
+		this.modelYear = modelYear;
+		this.price = price;
+		this.color = color;
+		this.fuelType = fuelType;
+		this.mileage = mileage;
+		this.mass = mass;
+		this.cylinders = cylinders;
+		this.gasTankCapacity = gasTankCapacity;
+		this.startType = startType;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public long getModelYear() {
+		return modelYear;
+	}
+	public void setModelYear(long modelYear) {
+		this.modelYear = modelYear;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public VehicleColor getColor() {
+		return color;
+	}
+	public void setColor(VehicleColor color) {
+		this.color = color;
+	}
+
+	public FuelType getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(FuelType fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public double getMileage() {
+		return mileage;
+	}
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+	public void setCylinders(int cylinders) {
+		this.cylinders = cylinders;
+	}
+
+	public double getGasTankCapacity() {
+		return gasTankCapacity;
+	}
+	public void setGasTankCapacity(double gasTankCapacity) {
+		this.gasTankCapacity = gasTankCapacity;
+	}
+
+	public StartMechanism getStartType() {
+		return startType;
+	}
+	public void setStartType(StartMechanism startType) {
+		this.startType = startType;
+	}
+	
+	 public String toCsvString() {
+	        return brand + "," + make + "," + modelYear + "," + price + "," + color + ","
+	               + fuelType + "," + mileage + "," + mass + "," + cylinders + ","
+	               + gasTankCapacity + "," + startType;
+	    }
 }
