fcee5157e74454d32a0ae3306ed1896f84095574
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-24 23:48:13 -0600

    Update Main.java
    Implemented functions

--- src/m/main/Main.java
+++ src/m/main/Main.java
@@ -4,20 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Instantiate vehicleManager, perform operations based on the requirements.
-		
-		//VehicleManager vehicleManager = new VehicleManager();
-		
+		VehicleManager vehicleManager = new VehicleManager();
+
 		// Read vehicle data from the vehcileList.csv file and initialize objects.
-		// TODO
+		vehicleManager.initializeStock("Files\\vehicleList.csv");
+
+		
 		
 		// Display all vehicle information.
-		// TODO
+		vehicleManager.displayAllVehicleInformation();
 		
-		// Display all car information.
-		// TODO
+		//Display all Car information
+		vehicleManager.displayAllCarInformation();
 		
 		// Display all motorbike information.
-		// TODO
+		vehicleManager.displayAllMotorBikeInformation();
 
 	}
 }
