5f654afbba80e7c7324a722efddb5cce82f62498
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-26 22:52:05 -0600

    Update Main.java
    Updated File address, program now properly reads file.

--- src/m/main/Main.java
+++ src/m/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		VehicleManager vehicleManager = new VehicleManager();
 
 		// Read vehicle data from the vehcileList.csv file and initialize objects.
-		vehicleManager.initializeStock("File\\vehicleList.csv");
+		vehicleManager.initializeStock("src/m/File/vehicleList.csv");
 
 		
 		
